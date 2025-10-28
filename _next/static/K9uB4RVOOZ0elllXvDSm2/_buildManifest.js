96ef5343f827c44a68d01a254beadaaaea4ff327
OdilCoda <twhtaboyevodilfair@gmail.com>
2025-10-28 23:33:51 +0500

    Add files via upload

---
+++ _next/static/K9uB4RVOOZ0elllXvDSm2/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/e96844022b5f7d6d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
